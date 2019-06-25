bdf9b21f3e9ca27d76ced39e700726282b26e9f0
Selin Gül <dselin26@gmail.com>
2019-06-25 16:18:33 +0300

    haftanın günlerini ekrana basan kod

---
+++ HaftaninGunleri.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Haftanın Günleri:");
+    String[] gunler={"Pazartesi" ,"Sali", "Carsamba", "Persembe", "Cuma", "Cumartesi", "Pazar"};
+    for(String i : gunler){
+      System.out.println(i);
+    }
+  }
+}
